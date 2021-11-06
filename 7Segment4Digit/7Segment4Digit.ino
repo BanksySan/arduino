03a0528a864b7fad95a0eddc5edeba855eaaa2d4
David Banks <BanksySan@googlemail.com>
2021-11-06 16:03:56 +0000

    7 segment working but the time on each character isn't constant.

--- 7Segment4Digit/7Segment4Digit.ino
+++ 7Segment4Digit/7Segment4Digit.ino
@@ -1,64 +1,51 @@
-#define A_SEGMENT 13
-#define B_SEGMENT 12
-#define C_SEGMENT 11
-#define D_SEGMENT 10
-#define E_SEGMENT 9
-#define F_SEGMENT 8
-#define G_SEGMENT 7
-#define DP_SEGMENT 6
-
-const byte DIGIT_PINS[] = { A5, A4, A3, A2 };
-const byte SEGMENT_PINS[] = { A_SEGMENT,
-                              B_SEGMENT,
-                              C_SEGMENT,
-                              D_SEGMENT,
-                              E_SEGMENT,
-                              F_SEGMENT,
-                              G_SEGMENT,
-                              DP_SEGMENT };
+const byte DIGITS[] = { A5, A4, A3, A2 };
+const byte SEGMENTS[] = { 13, 12, 11, 10, 9, 8, 7, 6 };
+const byte numericMaps[10] = { B00111111, B00000110, B01011011, B01001111, B01100110, B01101101, B01111101, B00000111, B01111111, B01100111 };
 
 void setup() {
   Serial.begin(9600);
-  do {
-    delay(5);
-  } while (!Serial);
-
+  while (!Serial) { ; }
+  delay(5);
   Serial.println("Initialised");
-  for (byte pin: DIGIT_PINS) {
-    Serial.println("  Digit pin:  " + String(pin));
+  delay(5);
+  
+  for (byte i = 0; i < 4; i++) {
+    byte pin = DIGITS[i];
     pinMode(pin, OUTPUT);
     digitalWrite(pin, LOW);
   }
 
-  for (byte pin: SEGMENT_PINS) {
-    Serial.println("Segment pin:  " + String(pin));
+  for (byte i = 0; i < 8; i++) {
+    byte pin = SEGMENTS[i];
     pinMode(pin, OUTPUT);
-    digitalWrite(pin, HIGH);
-    delay(500);
+    digitalWrite(pin, LOW);
   }
 }
 
 void loop() { 
-  for (byte pin: SEGMENT_PINS) {
-    digitalWrite(pin, LOW);
-  }
+  byte characters[4] = { numericMaps[8], numericMaps[9], numericMaps[6], numericMaps[7] };
+  writeDisplay(characters);
+}
 
-  for (byte pin: SEGMENT_PINS) {
-    Serial.println("Segment pin:  " + String(pin));
-    pinMode(pin, OUTPUT);
-    digitalWrite(pin, HIGH);
-    delay(500);
+void writeDisplay(byte digets[4]) {
+  for (byte i = 0; i < 4; i++) {
+    clearDisplay();
+    byte digit = digets[i];
+    setSegments(digit);
+    digitalWrite(DIGITS[i], LOW);
   }
 }
 
+void setSegments(byte state) {
+  for (byte i = 0; i < 8; i++) {
+    bool bit = bitRead(state, i);
+    byte pin = SEGMENTS[i];
+    digitalWrite(pin, bit);
+  }
+}
 
-
-// void serialEvent() {
-//   String incoming = Serial.readString();
-//   for (byte i = 0; i < 4; i++) {
-//     bool state = (char) incoming[i] == '*';
-  
-//     digitalWrite(DIGIT_PINS[i], state);
-//     Serial.print(String(i) + ":" + String(state) + "\n");
-//   }
-// }
+void clearDisplay() {
+  for (byte pin : DIGITS) {
+    digitalWrite(pin, HIGH);
+  }
+}
